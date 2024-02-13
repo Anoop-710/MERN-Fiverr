7b2c3e0c46de983bdf112faf7e4fa1e0a038b0ee
anoop <anoopaathreya@gmail.com>
2024-02-13 14:07:51 +0530

    fix

--- src/Pages/Messages/Messages.jsx
+++ src/Pages/Messages/Messages.jsx
@@ -1,17 +1,11 @@
-import React from 'react'
-import "./Messages.scss"
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./Messages.scss";
+import { Link } from "react-router-dom";
 const Messages = () => {
-
-  const currentUser = {
-    id: 1,
-    name: "John Doe",
-    isSeller: true,
-  };
-
-  const message = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt officia eum cumque doloremque nihil aliquam repellat a, quo nam blanditiis numquam unde sunt ullam ab, possimus ut deserunt mollitia nostrum';
+  const message =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt officia eum cumque doloremque nihil aliquam repellat a, quo nam blanditiis numquam unde sunt ullam ab, possimus ut deserunt mollitia nostrum";
   return (
-    <div className='messages'>
+    <div className="messages">
       <div className="container">
         <div className="title">
           <h1>Messages</h1>
@@ -24,66 +18,73 @@ const Messages = () => {
             <th>Action</th>
           </tr>
 
-          <tr className='active'>
+          <tr className="active">
+            <td>John Doe</td>
             <td>
-              John Doe
+              <Link to="/message/1234" className="link">
+                {message.slice(0, 100)}...
+              </Link>
             </td>
-            <td><Link to="/message/1234" className='link'>{message.slice(0,100)}...</Link></td>
             <td>1 day ago</td>
             <td>1234</td>
             <td>
-              <button>Mark  as Read</button>
+              <button>Mark as Read</button>
             </td>
           </tr>
 
-          <tr className='active'>
+          <tr className="active">
+            <td>John Doe</td>
             <td>
-              John Doe
+              <Link to="/message/1234" className="link">
+                {message.slice(0, 100)}...
+              </Link>
             </td>
-            <td><Link to="/message/1234" className='link'>{message.slice(0,100)}...</Link></td>
             <td>1 day ago</td>
             <td>1234</td>
             <td>
-              <button>Mark  as Read</button>
-            </td>
-          </tr>
-
-          <tr>
-            <td>
-              John Doe
-            </td>
-            <td><Link to="/message/1234" className='link'>{message.slice(0,100)}...</Link></td>
-            <td>1 day ago</td>
-            <td>1234</td>
-            <td>
+              <button>Mark as Read</button>
             </td>
           </tr>
 
           <tr>
+            <td>John Doe</td>
             <td>
-              John Doe
+              <Link to="/message/1234" className="link">
+                {message.slice(0, 100)}...
+              </Link>
             </td>
-            <td><Link to="/message/1234" className='link'>{message.slice(0,100)}...</Link></td>
             <td>1 day ago</td>
             <td>1234</td>
-            <td>
-            </td>
+            <td></td>
           </tr>
 
           <tr>
+            <td>John Doe</td>
             <td>
-              John Doe
+              <Link to="/message/1234" className="link">
+                {message.slice(0, 100)}...
+              </Link>
             </td>
-            <td><Link to="/message/1234" className='link'>{message.slice(0,100)}...</Link></td>
             <td>1 day ago</td>
             <td>1234</td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td>John Doe</td>
             <td>
+              <Link to="/message/1234" className="link">
+                {message.slice(0, 100)}...
+              </Link>
             </td>
+            <td>1 day ago</td>
+            <td>1234</td>
+            <td></td>
           </tr>
         </table>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Messages
+export default Messages;
